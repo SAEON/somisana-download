8eb4b653a7fc78243972cc2e6f3f8efd1c02fc87
GRautenbach <g.rautenbach@saeon.nrf.ac.za>
2026-05-25 14:15:16 +0200

    Update the URL for downloading the CSIR products.

--- download_tools/csir.py
+++ download_tools/csir.py
@@ -6,7 +6,7 @@ def download_phytoplankton_flags(date,output_dir):
     """
     Function to download CSIR phytoplankton flags and save as NetCDF file. CSIR 
     disseminates the data freely using the link below: 
-        https://www.ocims.gov.za/data/s3olci/s3-phytoplankton-south_africa
+        https://ocims.csir.co.za/data/s3olci/s3-phytoplankton-south_africa
 
     Parameters
     ----------
@@ -16,7 +16,7 @@ def download_phytoplankton_flags(date,output_dir):
         Directory to save downloaded phytoplankton flags to as a NetCDF file.
     """
     # Build file path
-    url = "https://www.ocims.gov.za/data/s3olci/s3-phytoplankton-south_africa"
+    url = "https://ocims.csir.co.za/data/s3olci/s3-phytoplankton-south_africa"
     file_in = os.path.join(url, f"phyto_south_africa_{date.strftime('%Y%m%d')}.tif" )
     try:
         print(f"\nOpening tif file: {file_in}")
@@ -49,7 +49,7 @@ def download_chlorophyll_conc(date,output_dir):
         Directory to save downloaded phytoplankton flags to as a NetCDF file.
     """
     # Build file path
-    url = "https://www.ocims.gov.za/data/s3olci/s3-chl-south_africa"
+    url = "https://ocims.csir.co.za/data/s3olci/s3-chl-south_africa/"
     file_in = os.path.join(url, f"chl_south_africa_{date.strftime('%Y%m%d')}.tif" )
     try:
         print(f"\nOpening tif file: {file_in}")
